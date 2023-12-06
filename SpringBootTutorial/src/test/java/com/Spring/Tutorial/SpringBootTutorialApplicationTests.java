1a067e4ee17caa75fcace93e7b8211d6c07cadf2
NabilAsyraf2904 <nabilasyraf215@gmail.com>
2023-12-06 10:23:09 +0800

    CreateSpring
    Set SpringBoot

---
+++ SpringBootTutorial/src/test/java/com/Spring/Tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
