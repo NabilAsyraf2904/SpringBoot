49313c177fa14b781e63047e3837ba84ac7e36da
NabilAsyraf2904 <nabilasyraf215@gmail.com>
2023-12-12 13:59:12 +0800

    Update IndexController.java
    delete function success

--- SpringBootTutorial/src/main/java/com/Spring/Tutorial/Controller/IndexController.java
+++ SpringBootTutorial/src/main/java/com/Spring/Tutorial/Controller/IndexController.java
@@ -2,7 +2,6 @@ package com.Spring.Tutorial.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +39,7 @@ public class IndexController {
         return "redirect:/Index"; 
     }
 	
-	@DeleteMapping("/delete/{timesheetID}")
+	@PostMapping("/delete/{timesheetID}")
     public String deleteInventoryItem(@PathVariable int timesheetID, RedirectAttributes redirectAttributes) {
         inv.deleteById(timesheetID);
         redirectAttributes.addFlashAttribute("message", "Timesheet deleted successfully");
